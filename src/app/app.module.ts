a29d617a171c1e5226224bb8948ce267f35f9f1f
Lucas Ribeiro <lucas.ribeiro@dbccompany.com.br>
2021-06-26 15:11:04 -0300

    criação de telas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateEdicaoDisciplinaComponent } from './create-edicao-disciplina/create-edicao-disciplina.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { EditEdicaoDisciplinaComponent } from './edit-edicao-disciplina/edit-edicao-disciplina.component';
+import { ListEdicaoDisciplinaComponent } from './list-edicao-disciplina/list-edicao-disciplina.component';
+import { ListStudentsDisciplinaComponent } from './list-students-disciplina/list-students-disciplina.component';
+import { ListStudentsDataComponent } from './list-students-data/list-students-data.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateEdicaoDisciplinaComponent
+    CreateEdicaoDisciplinaComponent,
+    EditEdicaoDisciplinaComponent,
+    ListEdicaoDisciplinaComponent,
+    ListStudentsDisciplinaComponent,
+    ListStudentsDataComponent,
   ],
   imports: [
     BrowserModule,
@@ -22,7 +32,8 @@ import {MatInputModule} from '@angular/material/input';
     MatFormFieldModule,
     MatSelectModule,
     MatButtonModule,
-    MatInputModule
+    MatInputModule,
+    MatTableModule
   ],
 
   providers: [],
